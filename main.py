aaf0c047b31a420d8c38e75bb8fabfa3b03f15c7
bikramadittyamondal0099-lgtm <bikramadittyamondal0099@gmail.com>
2026-07-24 18:38:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from parser import parse_file
+from tracer import run
+from database import clear, show_records
+TARGET_FILE = "test sample.py"
+def main():
+    print("=" * 50)
+    print("        PYCHRONICLE")
+    print("=" * 50)
+    clear()
+
+    print("\nReading Python File\n")
+
+    # Parse the target file
+    code = parse_file(TARGET_FILE)
+
+    print("\nStarting Execution Trace\n")
+    run(code)
+    print("\nExecution History\n")
+    show_records()
+    print("\nPyChronicle Finished Successfully.")
+
+if __name__ == "__main__":
+    main()
