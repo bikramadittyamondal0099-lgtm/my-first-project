1861a2898d2c86882a858273a11f4e6c69da0646
bikramadittyamondal0099-lgtm <bikramadittyamondal0099@gmail.com>
2026-07-31 15:43:59 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,31 +1,16 @@
 import ast
-
-
 class VariableTracker(ast.NodeVisitor):
-
     def visit_Assign(self, node):
-
         for target in node.targets:
-
             if isinstance(target, ast.Name):
-
                 print(
                     f"Variable Found : {target.id} (Line {node.lineno})"
                 )
-
         self.generic_visit(node)
-
-
 def parse_file(filename):
-
     with open(filename, "r") as file:
-
         code = file.read()
-
     tree = ast.parse(code)
-
     tracker = VariableTracker()
-
     tracker.visit(tree)
-
     return code
